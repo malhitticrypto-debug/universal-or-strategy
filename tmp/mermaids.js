8d0b6ea550a13199a7157f653e6a6e7a7bc4ed59
AI M. Khalid <mkalhitti@gmail.com>
2026-04-22 09:22:32 -0700

    chore: clean up repo hygiene and untrack noise files [Level 5]

--- tmp/mermaids.js
+++
@@ -1,8 +0,0 @@
-const fs=require('fs');
-const c=fs.readFileSync('docs/arena_dashboard.html','utf8');
-const m=c.match(/<div class="mermaid\s*">([\s\S]*?)<\/div>/g) || c.match(/<div[^>]*class=[^>]*mermaid[^>]*>([\s\S]*?)<\/div>/g);
-if(m){
-    m.forEach((b,i)=>console.log(`\n\n--- BLOCK ${i} ---\n`+b));
-}else{
-    console.log('No blocks');
-}
