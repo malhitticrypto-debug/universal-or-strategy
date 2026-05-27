07d64ae043374917232542c2f9c91346017640fd
mdasdispatch-hash <mkalhitti@gmail.com>
2026-05-27 14:43:33 -0700

    style: Add CLS compliance attribute (CA1014) - resolves 98 Codacy issues

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+// <copyright file="AssemblyInfo.cs" company="BMad">
+// Copyright (c) BMad. All rights reserved.
+// </copyright>
+// Assembly-level attributes for V12 Universal OR Strategy
+
+using System;
+
+[assembly: CLSCompliant(true)]
+
+// Made with Bob
